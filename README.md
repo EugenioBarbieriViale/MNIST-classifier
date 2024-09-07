375c67293dc102ced85c880317005063f2937ac0
EugenioBarbieriViale <eugenio.barbieri.viale@gmail.com>
2024-09-07 17:54:28 +0200

    new

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # MNIST-classifier
+
+https://towardsdatascience.com/mnist-handwritten-digits-classification-from-scratch-using-python-numpy-b08e401c4dab
+
+https://github.com/karynaur/mnist-from-numpy/blob/main/mnist.py
+
+https://github.com/apssouza22/neuroevolution
+
+https://github.com/numpy/numpy-tutorials/blob/main/content/tutorial-deep-learning-on-mnist.md
+
+https://github.com/iamtrask/Grokking-Deep-Learning
+
+------------------------------------
+
+https://machinelearningmastery.com/implement-backpropagation-algorithm-scratch-python/
+
+https://pyimagesearch.com/2021/05/06/backpropagation-from-scratch-with-python/
---
+++ get_data.py
@@ -0,0 +1,25 @@
+# Download mnist dataset here: 
+# https://github.com/cvdfoundation/mnist
+
+
+import gzip
+import numpy as np
+
+path = "data/"
+
+train_image_file = path + "train-images-idx3-ubyte.gz"
+train_label_file = path + "train-labels-idx1-ubyte.gz"
+
+test_image_file  = path + "t10k-images-idx3-ubyte.gz"
+test_label_file  = path + "t10k-labels-idx1-ubyte.gz"
+
+def process(name_file):
+    with open(name_file, "rb") as f:
+        data = f.read()
+    return np.frombuffer(gzip.decompress(data), dtype=np.uint8).copy()
+
+X_train = process(train_image_file)[0x10:].reshape((-1, 28, 28))
+Y_train = process(train_label_file)[8:]
+
+X_test = process(test_image_file)[0x10:].reshape((-1, 28, 28))
+Y_test = process(test_label_file)[8:]
