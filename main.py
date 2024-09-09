b2da75be7be51512f42777d5823b767d93c364ab
EugenioBarbieriViale <eugenio.barbieri.viale@gmail.com>
2024-09-09 14:58:53 +0200

    new

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ output_size = 10
 
 epochs = 1
 learn_rate = 1e-2
+h = 1e-2
 
 def sigmoid(x):
     return (1/(1 + np.exp(-x)))
@@ -14,21 +15,29 @@ def sigmoid(x):
 def d_sigmoid(x):
     return np.exp(-x)/(np.exp(-x)**2 + 1)
 
+def forward(w1, w2):
+    hidden_net = np.dot(input_layer, w1)
+    hidden_out = sigmoid(hidden_net)
 
-def init_weights(rows, cols):
-    return np.random.rand(rows, cols)
+    output_net = np.dot(hidden_out, w2)
+    output_out = sigmoid(output_net)
 
-w1 = init_weights(784,128)
-w2 = init_weights(128,10)
+    cost = ((output_out - target) ** 2).sum()
+    return cost
 
-for i in range(epochs):
+w1 = np.random.rand(784, 128)
+w2 = np.random.rand(128,  10)
+
+g1 = np.zeros((784, 128))
+g2 = np.zeros((128,  10))
+
+# for i in range(len(X_train)):
+for i in range(1):
     label = Y_train[i]
     input_layer = X_train[i]
 
-    hidden_layer = sigmoid(np.dot(input_layer, w1))
-    output_layer = sigmoid(np.dot(hidden_layer, w2))
+    target = [1 if n == (label-1) else 0 for n in range(output_size)]
 
-    target = [1 if (n+1) == label else 0 for n in range(output_size)]
-    loss = (output_layer - target) * d_sigmoid(output_layer)
-    cost = loss.sum()
-
+    for j in range(epochs):
+        cost = forward(w1, w2)
+        print(cost)
